4cc8919d86f44b61691c44e14b6df46ac2d7d464
Charalampos Gkikas <gkikas@idika.gr>
2019-06-09 11:53:01 +0300

    psutil for managing processes

--- daemon_bot.py
+++ daemon_bot.py
@@ -1,15 +1,30 @@
 from pathlib import Path
+import io
 
 from daemoniker import Daemonizer
-from daemoniker.exceptions import DaemonikerException
+import psutil
 
 import reddit_bot
+import time
+
+def isDaemonRunning(pid_file):
+    if pid_file.exists():
+        with io.open(pid_file, 'r', enconding='ascii') as f_obj:
+            pid = int(f_obj.read().strip())
+        if psutil.pid_exists(pid):
+            p = psutil.Process(pid=pid)
+            with p.oneshot():
+                name = p.name()
+                status = p.status()
+            print("Found process:", name, status)
 
 with Daemonizer() as (is_setup, daemonizer):
     if is_setup:
         # This code is run before daemonization.
         pid_file = Path(__file__).parent / 'f1_bot.pid'
         path_pid = str(pid_file)
+        isDaemonRunning(pid_file)
+        
 
     # We need to explicitly pass resources to the daemon; other variables
     # may not be correct
@@ -20,7 +35,5 @@ with Daemonizer() as (is_setup, daemonizer):
         print("The process was forked to the background")
 
 # We are now daemonized, and the parent just exited.
-try:
-    reddit_bot.main()
-except DaemonikerException:
-    pid_file.unlink()
+#reddit_bot.main()
+time.sleep(300)
